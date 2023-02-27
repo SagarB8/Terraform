9830e3854481eab5f86b49e00ff98fe829aa7d0b
SagarB8 <120292860+SagarB8@users.noreply.github.com>
2023-02-27 17:36:11 +0530

    Update ec2.tf
    Added one more instance query to create new instance t2.micro

--- ec2.tf
+++ ec2.tf
@@ -9,13 +9,28 @@ terraform {
 
 resource "aws_instance" "terraform" {
   ami           = "ami-0caf778a172362f1c"
+  instance_type = "t2.medium"
+  key_name = "Docker"
+  vpc_security_group_ids = ["sg-03316bd636d90cc14"]
+  subnet_id = "subnet-0a0778948a08ae08a"
+  associate_public_ip_address = true
+  availability_zone = "ap-south-1"
+
+  tags = {
+    Name = "Finance-Project_Jenkins"
+  }
+}
+
+resource "aws_instance" "terraform" {
+  ami           = "ami-0caf778a172362f1c"
   instance_type = "t2.micro"
   key_name = "Docker"
   vpc_security_group_ids = ["sg-03316bd636d90cc14"]
   subnet_id = "subnet-0a0778948a08ae08a"
   associate_public_ip_address = true
+  availability_zone = "ap-south-1"
 
   tags = {
-    Name = "Terraform hands-on"
+    Name = "Monitoring-tool"
   }
 }
